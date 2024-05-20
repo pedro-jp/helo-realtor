fc17b988918cfab18c271bfdd675e5578a7798b6
pedro-jp <joaopedroc035@gmail.com>
2024-05-19 22:23:36 -0300

    refactor: add destructure const

--- backend/src/controllers/image/ListImagesController.ts
+++ backend/src/controllers/image/ListImagesController.ts
@@ -3,9 +3,10 @@ import { ListImagesService } from '../../services/image/ListImagesService';
 
 export class ListImagesController {
   async handle(req: Request, res: Response) {
+    const { imovelId } = req.body;
     const listImagesService = new ListImagesService();
 
-    const images = await listImagesService.execute(req.body.imovelId);
+    const images = await listImagesService.execute(imovelId);
 
     return res.json(images);
   }
