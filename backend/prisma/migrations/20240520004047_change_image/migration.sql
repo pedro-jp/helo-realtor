a85ae098e6ba01ca7f251e2935793150398b03c9
pedro-jp <joaopedroc035@gmail.com>
2024-05-19 22:22:31 -0300

    feat: remove banner and add image realtion

---
+++ backend/prisma/migrations/20240520004047_change_image/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `banner` on the `imoveis` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "imoveis" DROP COLUMN "banner";
