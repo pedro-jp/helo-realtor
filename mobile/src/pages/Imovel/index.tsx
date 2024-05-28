8cc2e2719e42ee907e2803f0610358fecc17c464
pedro-jp <joaopedroc035@gmail.com>
2024-05-27 23:33:40 -0300

    feat: add put route

--- mobile/src/pages/Imovel/index.tsx
+++ mobile/src/pages/Imovel/index.tsx
@@ -1,11 +1,19 @@
-import { Image, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
+import {
+  Button,
+  Image,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 import { useRoute } from '@react-navigation/native';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { api } from '../../services/api';
 import { StatusBar } from 'expo-status-bar';
 import Carousel from '../../components/Carousel';
 import { Feather } from '@expo/vector-icons';
 import * as S from './styles';
+import { UserProps } from '../../interfaces';
+import { AuthContext } from '../../contexts/AuthContext';
 
 type ImovelType = {
   name: string;
@@ -20,12 +28,25 @@ type ImovelType = {
   banheiros: string;
   area: string;
   garagem: string;
+  categoryId: string;
+  active: boolean;
 };
 
 export default function Imovel() {
   const Routes = useRoute();
   const { imovelId } = Routes.params as { imovelId: string };
   const [imovel, setImovel] = useState<ImovelType>({} as ImovelType);
+  const { user } = useContext(AuthContext);
+
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState('');
+  const [local, setLocal] = useState('');
+  const [quartos, setQuartos] = useState('');
+  const [banheiros, setBanheiros] = useState('');
+  const [area, setArea] = useState('');
+  const [garagem, setGaragem] = useState('');
+  const [active, setActive] = useState(false);
 
   useEffect(() => {
     loadImovel();
@@ -34,11 +55,49 @@ export default function Imovel() {
   async function loadImovel() {
     try {
       const response = await api.get(`/imovel/${imovelId}`);
+      setName(response?.data.name);
+      setDescription(response?.data.description);
+      setPrice(response?.data.price);
+      setLocal(response?.data.local);
+      setQuartos(response?.data.quartos);
+      setBanheiros(response?.data.banheiros);
+      setArea(response?.data.area);
+      setGaragem(response?.data.garagem);
+      setActive(response?.data.active);
       setImovel(response?.data);
     } catch (error) {
       console.log(error);
     }
   }
+
+  async function handleUpdate(categoryId: string) {
+    try {
+      const response = await api.put(`/imovel/${imovelId}`, {
+        name,
+        description,
+        price,
+        local,
+        quartos,
+        banheiros,
+        area,
+        garagem,
+        active,
+        ownerId: user.id,
+        categoryId,
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.response.data ?? error.message);
+    }
+  }
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleFocus = () => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
 
   const IMAGE_URL =
     'https://images.pexels.com/photos/439391/pexels-photo-439391.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
@@ -56,35 +115,36 @@ export default function Imovel() {
         {imovel && (
           <S.StyledContentView>
             <S.StyledRowView>
-              <S.StyledText>Imóvel: {imovel?.name}</S.StyledText>
-              <TouchableOpacity>
-                <TouchableOpacity>
-                  <Feather name='edit-2' size={20} color='white' />
-                </TouchableOpacity>
-              </TouchableOpacity>
-            </S.StyledRowView>
-            <S.StyledRowView>
-              <S.StyledText>
-                Descrição: {'\n'} {imovel?.description}
-              </S.StyledText>
-              <TouchableOpacity>
+              <S.StyledTitle>Nome:</S.StyledTitle>
+              <S.StyledInput value={name} onChangeText={setName} />
+              <TouchableOpacity onPress={handleFocus}>
                 <Feather name='edit-2' size={20} color='white' />
               </TouchableOpacity>
             </S.StyledRowView>
             <S.StyledRowView>
-              <S.StyledText>
+              <S.StyledInput onChangeText={setDescription} ref={inputRef}>
+                Descrição: {'\n'} {imovel?.description}
+              </S.StyledInput>
+              <TouchableOpacity onPress={handleFocus}>
+                <Feather name='edit-2' size={20} color='white' />
+              </TouchableOpacity>
+            </S.StyledRowView>
+            <S.StyledRowView>
+              <S.StyledInput onChangeText={setPrice} ref={inputRef}>
                 Valor:{' '}
                 {Number(imovel?.price).toLocaleString('pt-br', {
                   style: 'currency',
                   currency: 'BRL',
                 })}
-              </S.StyledText>
+              </S.StyledInput>
               <TouchableOpacity>
                 <Feather name='edit-2' size={20} color='white' />
               </TouchableOpacity>
             </S.StyledRowView>
             <S.StyledRowView>
-              <S.StyledText>Local: {imovel?.local}</S.StyledText>
+              <S.StyledInput onChangeText={setLocal} ref={inputRef}>
+                Local: {imovel?.local}
+              </S.StyledInput>
               <TouchableOpacity>
                 <Feather name='edit-2' size={20} color='white' />
               </TouchableOpacity>
@@ -94,7 +154,9 @@ export default function Imovel() {
 
             {imovel?.quartos === '' ? null : (
               <S.StyledRowView>
-                <S.StyledText>Quartos: {imovel?.quartos}</S.StyledText>
+                <S.StyledInput onChangeText={setQuartos}>
+                  Quartos: {imovel?.quartos}
+                </S.StyledInput>
                 <TouchableOpacity>
                   <Feather name='edit-2' size={20} color='white' />
                 </TouchableOpacity>
@@ -102,7 +164,9 @@ export default function Imovel() {
             )}
             {imovel?.banheiros === '' ? null : (
               <S.StyledRowView>
-                <S.StyledText>Banheiros: {imovel?.banheiros}</S.StyledText>
+                <S.StyledInput onChangeText={setBanheiros}>
+                  Banheiros: {imovel?.banheiros}
+                </S.StyledInput>
                 <TouchableOpacity>
                   <Feather name='edit-2' size={20} color='white' />
                 </TouchableOpacity>
@@ -110,7 +174,9 @@ export default function Imovel() {
             )}
             {imovel?.area === '' ? null : (
               <S.StyledRowView>
-                <S.StyledText>Area: {imovel?.area} m²</S.StyledText>
+                <S.StyledInput onChangeText={setArea}>
+                  Area: {imovel?.area} m²
+                </S.StyledInput>
                 <TouchableOpacity>
                   <Feather name='edit-2' size={20} color='white' />
                 </TouchableOpacity>
@@ -118,7 +184,9 @@ export default function Imovel() {
             )}
             {imovel?.garagem === '' ? null : (
               <S.StyledRowView>
-                <S.StyledText>Garagem: {imovel?.garagem}</S.StyledText>
+                <S.StyledInput onChangeText={setGaragem}>
+                  Garagem: {imovel?.garagem}
+                </S.StyledInput>
                 <TouchableOpacity>
                   <Feather name='edit-2' size={20} color='white' />
                 </TouchableOpacity>
@@ -127,6 +195,10 @@ export default function Imovel() {
           </S.StyledContentView>
         )}
       </ScrollView>
+      <Button
+        onPress={() => handleUpdate(imovel?.categoryId)}
+        title='Atualizar'
+      />
     </S.StyledContainerView>
   );
 }
