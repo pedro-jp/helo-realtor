b20880d98a9edfd059aa13b1a03cb5fdc65e5bb4
pedro-jp <joaopedroc035@gmail.com>
2024-09-01 21:40:26 -0300

    feat: estilizacao card

--- front-model-1/app/components/cards/index.tsx
+++ front-model-1/app/components/cards/index.tsx
@@ -1,6 +1,7 @@
 import { getImoveis } from '@/app/services/getImoveis';
 import Image from 'next/image';
 import Link from 'next/link';
+import styles from './style.module.scss';
 
 export default async function Cards() {
   const imoveis = await getImoveis();
@@ -12,12 +13,29 @@ export default async function Cards() {
           <Link href={`/imovel/${imovel.id}`} key={imovel.id}>
             <h2>{imovel.name}</h2>
             {imovel.images && imovel.images.length > 0 && (
-              <Image
-                src={imovel.images[0]?.url}
-                alt={imovel.name}
-                width={500}
-                height={300}
-              />
+              <div className={styles.houseInfo}>
+                <div className={styles.houseImage}>
+                  <Image
+                    src={imovel.images[0].url}
+                    alt='House Image'
+                    layout='responsive'
+                    width={500}
+                    height={300}
+                  />
+                </div>
+
+                <div className={styles.housePrice}>
+                  <span>$500,000</span>
+                </div>
+
+                <ul className={styles.houseMeta}>
+                  <li>Somewhere</li>
+                  <li>
+                    158m<sup>2</sup>
+                  </li>
+                  <li>Big house</li>
+                </ul>
+              </div>
             )}
             <p>{imovel.description}</p>
           </Link>
