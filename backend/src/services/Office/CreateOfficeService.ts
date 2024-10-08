8e9b33d3eab36e38859e5aa13b7da6e5d6dfa1f9
pedro-jp <joaopedroc035@gmail.com>
2024-10-08 14:34:47 -0300

    fix: create office node fetch

--- backend/src/services/Office/CreateOfficeService.ts
+++ backend/src/services/Office/CreateOfficeService.ts
@@ -1,5 +1,6 @@
 import prismaClient from '../../prisma';
 
+// Define a tipagem do request
 interface OfficeRequest {
   name: string;
   ownerId: string;
@@ -8,6 +9,19 @@ interface OfficeRequest {
   address_city: string;
   description: string;
   email: string;
+}
+
+// Define a tipagem da resposta da API do Google Geocoding
+interface GoogleGeocodeResponse {
+  status: string;
+  results: Array<{
+    geometry: {
+      location: {
+        lat: number;
+        lng: number;
+      };
+    };
+  }>;
 }
 
 export class CreateOfficeService {
@@ -25,12 +39,14 @@ export class CreateOfficeService {
       `${address}, ${address_city}`
     );
 
+    // Gerar a URL baseada no nome do escritório
     const url = name
       .normalize('NFD')
       .replace(/[\u0300-\u036f]/g, '')
       .replace(/\s+/g, '')
       .replace(/[^\w-]+/g, '');
 
+    // Verifica se o escritório já existe
     const officeExists = await prismaClient.office.findFirst({
       where: {
         name,
@@ -44,6 +60,7 @@ export class CreateOfficeService {
     }
 
     try {
+      // Cria o escritório no banco de dados
       const office = await prismaClient.office.create({
         data: {
           name,
@@ -76,12 +93,18 @@ export class CreateOfficeService {
   ): Promise<{ lat: number; lng: number } | null> {
     try {
       const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+      // Importa o node-fetch dinamicamente
+      const fetch = (await import('node-fetch')).default;
+
       const response = await fetch(
         `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
           address
         )}&key=${apiKey}`
       );
-      const data = await response.json();
+      const data = (await response.json()) as GoogleGeocodeResponse;
+      console.log(data);
+
+      // Verifica se a resposta contém resultados válidos
       if (data.status === 'OK' && data.results.length > 0) {
         const { lat, lng } = data.results[0].geometry.location;
         return { lat, lng };
