2060a65765b055380b8e62bb52ccb4ee0459f63f
pedro-jp <joaopedroc035@gmail.com>
2024-12-04 12:18:25 -0300

    Remove unused imports and redundant background SVG, fix width style.

--- front-model-1-all/app/AppHome/page.tsx
+++ front-model-1-all/app/AppHome/page.tsx
@@ -8,7 +8,6 @@ import { motion, useAnimation } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 import Build from '../assets/img/build.jpg';
 import Image from 'next/image';
-import Loading from '../components/ParticlesLoading';
 import TsParticles from '../components/Particles';
 
 const AppHome = () => {
@@ -47,8 +46,6 @@ const AppHome = () => {
       officeName: office.name,
       officeId: office.id,
     }));
-
-  const bg = `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 200 200'%3E%3Cdefs%3E%3ClinearGradient id='a' gradientUnits='userSpaceOnUse' x1='100' y1='33' x2='100' y2='-3'%3E%3Cstop offset='0' stop-color='%23000' stop-opacity='0'/%3E%3Cstop offset='1' stop-color='%23000' stop-opacity='1'/%3E%3C/linearGradient%3E%3ClinearGradient id='b' gradientUnits='userSpaceOnUse' x1='100' y1='135' x2='100' y2='97'%3E%3Cstop offset='0' stop-color='%23000' stop-opacity='0'/%3E%3Cstop offset='1' stop-color='%23000' stop-opacity='1'/%3E%3C/linearGradient%3E%3C/defs%3E%3Cg fill='%23846b62' fill-opacity='0.6'%3E%3Crect x='100' width='100' height='100'/%3E%3Crect y='100' width='100' height='100'/%3E%3C/g%3E%3Cg fill-opacity='0.5'%3E%3Cpolygon fill='url(%23a)' points='100 30 0 0 200 0'/%3E%3Cpolygon fill='url(%23b)' points='100 100 0 130 0 100 200 100 200 130'/%3E%3C/g%3E%3C/svg%3E")`;
 
   return (
     <>
@@ -124,7 +121,7 @@ const AppHome = () => {
                 }}
                 className={style.map_container}
               >
-                <div style={{ width: '100vh' }}>
+                <div style={{ width: '100%' }}>
                   <h2 style={{ marginBottom: '20px' }}>
                     Localize o escritório mais próximo de você
                   </h2>
