84b2c34af17e2f116fab9395beb773bc9dd1658e
pedro-jp <joaopedroc035@gmail.com>
2024-12-04 20:18:06 -0300

    Update robotsTxtOptions with additional sitemaps and configure sitemapSize.

--- front-model-1-all/app/server-sitemap-index.xml/route.ts
+++ front-model-1-all/app/server-sitemap-index.xml/route.ts
@@ -1,22 +1,24 @@
 import { getServerSideSitemapIndex } from 'next-sitemap';
+import { OfficeType } from '../types';
 
 // Função para buscar todas as URLs dinâmicas (exemplo do backend)
 async function fetchOfficeUrls() {
   const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/offices`);
   const offices = await response.json();
 
-  return offices.map((office) => `/e/${office.url}`);
+  return offices.map((office: OfficeType) => `/e/${office.url}`);
 }
 
 export async function GET() {
   const officeUrls = await fetchOfficeUrls();
 
-  return getServerSideSitemapIndex({
-    // Todas as rotas dinâmicas devem ser incluídas aqui
-    sitemaps: [
-      ...officeUrls.map((url) => `${process.env.NEXT_PUBLIC_FRONT_URL}${url}`),
-    ],
-  });
+  // Gera o índice do sitemap
+  return getServerSideSitemapIndex(
+    officeUrls.map(
+      (url: string) =>
+        `${process.env.NEXT_PUBLIC_FRONT_URL}/sitemap-${url.replace('/e/', '')}.xml`
+    )
+  );
 }
 
 export const dynamic = 'force-dynamic'; // Garante que a geração será feita em tempo real
