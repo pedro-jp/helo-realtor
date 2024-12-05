33153f2d1c18d078b0d3124ba728c634eb9a4ce3
pedro-jp <joaopedroc035@gmail.com>
2024-12-05 13:37:20 -0300

    Added error handling in `main.py` and updated tests in `test_main.py`.

---
+++ front-model-1-all/next-sitemap.config.js
@@ -0,0 +1,8 @@
+// next-sitemap.config.js
+module.exports = {
+  siteUrl: 'https://imoveis.intg.com.br', // substitua com sua URL de produção
+  generateRobotsTxt: true, // caso queira gerar um arquivo robots.txt também
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000
+};
